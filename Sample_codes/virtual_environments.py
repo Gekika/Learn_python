f5458351cb8ac9faf503d1d0194267be0b935550
gekika <gkkaranja2@gmail.com>
2025-01-14 12:56:06 +0300

    creating a virtual environment

---
+++ Sample_codes/virtual_environments.py
@@ -0,0 +1,5 @@
+# The module used to create virtual environments is called venv
+# To create a virtual environment select / move to the directory of your choice and then run the venv module as a script with the directory name
+# Example in my Linux computer to create a virtual env called Sample one I run the following command:
+
+# python3 -m venv Sample_one 
