069e6cf4702a9b8a64329cfe72eafbae7bebfb2a
gekika <gkkaranja2@gmail.com>
2025-01-25 22:25:32 +0300

    Data structures in python

--- Sample_codes/data_structures.py
+++ Sample_codes/data_structures.py
@@ -1,2 +1,3 @@
 # methods of list objects
 list.append(x) -#  Add an item to the end of the list; equivalent to a[len(a):] = [x].
+list.extend(iterable) -# extends the list by adding all the items in the iterable
